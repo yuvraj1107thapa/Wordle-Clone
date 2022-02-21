dd620f1b4db1531980bb0dec10b7b6d10d196f80
Yuvraj Thapa <yuvrajthapa692@gmail.com>
2022-02-21 12:42:47 +0530

    Add/Remove letter from guess-word grid board

--- script.js
+++ script.js
@@ -15289,6 +15289,9 @@ const dictionary = [
     "shave"
 ]
 
+const WORD_LENGTH = 5;
+const guessGrid = document.querySelector("[data-guess-grid]");
+
 startInteraction();
 
 function startInteraction() {
@@ -15302,7 +15305,7 @@ function stopInteraction() {
 }
 
 function handleMouseClick(e) {
-    if(e.targe.matches("[data-key]")) {
+    if(e.target.matches("[data-key]")) {
         pressKey(e.target.dataset.key);
         return;
     }
@@ -15327,7 +15330,29 @@ function handleKeyPress(e) {
         return;
     }
     if(e.key.match(/^[a-z]$/)) {
-        pressKey();
-        return;
+        pressKey(e.key);
+        return
     }
+}
+
+function pressKey(key) {
+    const activeTiles = getActiveTiles();
+    if(activeTiles.length >= WORD_LENGTH) return;
+    const nextTile = guessGrid.querySelector(":not([data-letter])");
+    nextTile.dataset.letter = key.toLowerCase();
+    nextTile.textContent = key;
+    nextTile.dataset.state = "active";
+}
+
+function deleteKey() {
+    const activeTiles = getActiveTiles();
+    const lastTile = activeTiles[activeTiles.length - 1];
+    if(lastTile == null) return;
+    lastTile.textContent = "";
+    delete lastTile.dataset.state;
+    delete lastTile.dataset.letter;
+}
+
+function getActiveTiles() {
+    return guessGrid.querySelectorAll('[data-state="active"]');
 }
